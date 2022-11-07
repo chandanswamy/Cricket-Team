4732ddd7716a4323cfbf3a87aab23d2f17bb484a
chandanswamy <chandanswamy13214@gmail.com>
2022-11-07 16:57:44 +0000

    cricket team practice

---
+++ snakeCase_to_camelCase.js
@@ -0,0 +1,10 @@
+const convertDbObjectToResponseObject = (dbObject) => {
+  return {
+    playerId: dbObject.player_id,
+    playerName: dbObject.player_name,
+    jerseyNumber: dbObject.jersey_number,
+    role: dbObject.role,
+  };
+};
+
+module.exports = convertDbObjectToResponseObject;
